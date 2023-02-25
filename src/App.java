5e134176f3d96305071e5be5960f36268971369b
EncryptedZero <tcbutzine@gmail.com>
2023-02-25 02:23:08 -0600

    Now launches scene based on data.json file exists.

--- src/App.java
+++ src/App.java
@@ -1,39 +1,50 @@
 
+import Helper.FileHelper;
 import Tests.FileHelperTest;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
  
 public class App extends Application {
+
+    Stage mStage;
+    Scene mScene1, mScene2;
+
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Test - Check logs");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                // Can comment out
-                FileHelperTest test = new FileHelperTest();
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+    public void start(Stage pStage) {
+        this.mStage = pStage;
+
+        Label tScene1Label = new Label("File Already found.");
+        Button tScene1Button = new Button("Scene 2");
+        tScene1Button.setOnAction(e -> mStage.setScene(mScene2));
+        VBox tScene1Layout = new VBox(20);
+        tScene1Layout.getChildren().addAll(tScene1Label, tScene1Button);
+
+        Label tScene2Label = new Label("File not found.");
+        Button tScene2Button = new Button("Scene 1");
+        tScene2Button.setOnAction(e -> mStage.setScene(mScene1));
+        VBox tScene2Layout = new VBox(20);
+        tScene2Layout.getChildren().addAll(tScene2Label, tScene2Button);
   
         /*
         Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
         Scene scene = new Scene(root);
         */
   
-        Scene scene = new Scene(root, 300, 250);
-  
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        mScene1 = new Scene(tScene1Layout, 300, 250);
+        mScene2 = new Scene(tScene2Layout, 300, 250);
+
+        this.mStage.setTitle("Hello World!");
+        if(FileHelper.dataFileExists()){
+            this.mStage.setScene(mScene1);
+        }
+        else{
+            this.mStage.setScene(mScene2);
+        }
+        this.mStage.show();
     }
  
  public static void main(String[] args) {
