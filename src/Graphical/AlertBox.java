5d19e4a0e809da20b0f8ff0675ac7b1eca427daa
EncryptedZero <tcbutzine@gmail.com>
2023-02-25 15:00:03 -0600

    Created AlertBox class and ConfirmBox class

---
+++ src/Graphical/AlertBox.java
@@ -0,0 +1,37 @@
+package Graphical;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * A class meant to make working with Alert Boxes easy
+ */
+public class AlertBox {
+    
+    public static void display(String pTitle, String pMessage){
+        Stage mStage = new Stage();
+
+        // Forces focus to this window
+        mStage.initModality(Modality.APPLICATION_MODAL);
+        mStage.setTitle(pTitle);
+        mStage.setMinWidth(200);
+
+        Label mLabelMessage = new Label(pMessage);
+
+        Button mCloseButton = new Button("Close");
+        mCloseButton.setOnAction(e -> mStage.close());
+
+        VBox mVBox1 = new VBox(10);
+        mVBox1.getChildren().addAll(mLabelMessage, mCloseButton);
+        mVBox1.setAlignment(Pos.CENTER);
+
+        Scene mAlertScene = new Scene(mVBox1);
+        mStage.setScene(mAlertScene);
+        mStage.showAndWait();
+    }
+}
---
+++ src/Graphical/ConfirmBox.java
@@ -0,0 +1,47 @@
+package Graphical;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * A class meant to make working with Comfirm Boxes easy
+ */
+public class ConfirmBox {
+
+    public static boolean mConfirmed;
+    
+    public static void display(String pTitle, String pMessage){
+        Stage mStage = new Stage();
+
+        // Forces focus to this window
+        mStage.initModality(Modality.APPLICATION_MODAL);
+        mStage.setTitle(pTitle);
+        mStage.setMinWidth(200);
+
+        Label mLabelMessage = new Label(pMessage);
+
+        Button mYesButton = new Button("Yes");
+        mYesButton.setOnAction(e -> {
+            mConfirmed = true;
+            mStage.close();
+        });
+        Button mNoButton = new Button("No");
+        mNoButton.setOnAction(e -> {
+            mConfirmed = false;
+            mStage.close();
+        });
+
+        VBox mVBox1 = new VBox(10);
+        mVBox1.getChildren().addAll(mLabelMessage, mYesButton, mNoButton);
+        mVBox1.setAlignment(Pos.CENTER);
+
+        Scene mAlertScene = new Scene(mVBox1);
+        mStage.setScene(mAlertScene);
+        mStage.showAndWait();
+    }
+}
